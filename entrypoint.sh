d4dd05ea03ce1214b0ac526cf8aad8bfa8c3ca29
linzjian666 <63142068+linzjian666@users.noreply.github.com>
2023-04-15 08:50:50 +0800

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ NEZHA_SERVER= # 哪吒三个参数，不需要的话可以留空，删除或在�
 NEZHA_PORT=
 NEZHA_KEY=2
 
-# ARGO_AUTH='{"AccountTag":"11152cff1c47149f73e0fce59e767bec","TunnelSecret":"WHe1YkUFNL6W1uebF0tFnwkBbHrmUfJClOFJLWsWTn8=","TunnelID":"61ad547f-2bec-46ea-903c-ac4e46e1d35b"}' # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-# ARGO_DOMAIN=agro-vvtss2.linzjian666vv.ml
+# ARGO_AUTH='{"AccountTag":"11152cff1c47149f73e0fce59e767bec","TunnelSecret":"ukjnd6Ap38N+QBmbiaJSK6mXBteU1QCYKf5aiyMFJo8=","TunnelID":"9192badc-e006-4224-ad1f-2c54a2cccbeb"} ' # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
+# ARGO_DOMAIN=agro-vvtss3.linzjian666vv.ml
 
 generate_argo() {
   cat > argo.sh << ABC
